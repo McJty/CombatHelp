3deae1ef676cd3c5a2a2bac29356d4f0a8ca19e0
McJty <mcjty1@gmail.com>
2016-11-07 13:28:31 +0100

    Made the toolbar restore action less strict. If it can't find an item with same NBT it will ignore NBT and try again

--- src/main/java/mcjty/combathelp/varia/Tools.java
+++ src/main/java/mcjty/combathelp/varia/Tools.java
@@ -21,10 +21,19 @@ public class Tools {
                 return i;
             }
         }
+        for (int i = startfrom; i < player.inventory.mainInventory.length; ++i) {
+            if (player.inventory.mainInventory[i] != null && stackEqualExactNoNBT(stack, player.inventory.mainInventory[i])) {
+                return i;
+            }
+        }
         return -1;
     }
 
     private static boolean stackEqualExact(ItemStack stack1, ItemStack stack2) {
         return stack1.getItem() == stack2.getItem() && (!stack1.getHasSubtypes() || stack1.getMetadata() == stack2.getMetadata()) && ItemStack.areItemStackTagsEqual(stack1, stack2);
+    }
+
+    private static boolean stackEqualExactNoNBT(ItemStack stack1, ItemStack stack2) {
+        return stack1.getItem() == stack2.getItem() && (!stack1.getHasSubtypes() || stack1.getMetadata() == stack2.getMetadata());
     }
 }
