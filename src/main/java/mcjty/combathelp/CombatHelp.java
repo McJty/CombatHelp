dfb8f56f9ec16a953ea7726da66c7501c300e899
McJty <mcjty1@gmail.com>
2017-01-04 18:49:42 +0100

    1.1.0

--- src/main/java/mcjty/combathelp/CombatHelp.java
+++ src/main/java/mcjty/combathelp/CombatHelp.java
@@ -20,7 +20,7 @@ import java.io.File;
         version = CombatHelp.VERSION)
 public class CombatHelp {
     public static final String MODID = "combathelp";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.1.0";
     public static final String MIN_FORGE10_VER = "12.18.1.2082";
     public static final String MIN_FORGE11_VER = "13.19.0.2157";
     public static final String COMPATLAYER_VER = "0.0.35alpha";
